c6dc33d90d83618251014ec5ba13dbc69a8f54ef
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-07-05 17:50:03 +0200

    create serializers

---
+++ accounts/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import User, Organisation
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['user_id', 'first_name', 'last_name', 'email', 'phone', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password'],
+            phone=validated_data.get('phone', '')
+        )
+        return user
+
+class OrganisationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organisation
+        fields = ['org_id', 'name', 'description']
