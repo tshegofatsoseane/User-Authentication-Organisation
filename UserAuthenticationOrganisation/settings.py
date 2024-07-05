29d383ae744adf79a3bf824aaaefebe0bba68c65
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-07-05 17:52:24 +0200

    configure jwt

--- UserAuthenticationOrganisation/settings.py
+++ UserAuthenticationOrganisation/settings.py
@@ -127,3 +127,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
